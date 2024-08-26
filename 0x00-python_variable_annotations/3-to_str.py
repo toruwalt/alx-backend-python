479d73c3fda38bf0ecd7070dc8ebe095c4869200
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-26 23:41:36 +0100

    Python Annotation - Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""String representation of the float with type-annotation"""
+
+
+def to_str(n: float) -> str:
+    """String Representation
+
+    Arguments:
+      n: type of float
+
+    Returns:
+      str_rep: type of string
+    """
+
+    str_rep: str = str(n)
+    return str_rep
