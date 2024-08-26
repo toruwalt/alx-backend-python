d2f3ae562b1c04c6afc547f55db70061e2e6f01a
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-26 22:58:37 +0100

    Python Annotation - Task 0

---
+++ 0-add.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float):
+    """
+    Performs Addition
+    a: The first argument of type float
+    b: The second argument of type float
+
+    Returns:
+    Sum of type float
+    """
+    c: float = a + b
+    return c
