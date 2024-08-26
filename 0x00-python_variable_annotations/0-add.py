c066fc212d49c3e629ef61242aacc40845d33633
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-26 23:13:09 +0100

    Python Annotation - Task 0:Attempt 6

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+"""Python Annoation"""
 
 def add(a: float, b: float) -> float:
     """
     Performs Addition
-    a: The first argument of type float
-    b: The second argument of type float
+      a: The first argument of type float
+      b: The second argument of type float
 
     Returns:
-    Sum of type float
+      Sum of type float
     """
     c: float = a + b
     return c
