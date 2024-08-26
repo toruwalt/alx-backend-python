59d702a5256cebc726024d1357e254945d30ee39
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-26 23:35:12 +0100

    Python Annotation - Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Using Math module with Annotations"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Floor a float
+
+    Arguments:
+        n: type float
+
+    Returns:
+        floor: type float
+    """
+
+    floor: int = math.floor(n)
+    return floor
