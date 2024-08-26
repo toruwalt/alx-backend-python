34577201cb8cd001c5a3d2c533c6828bbee29ce4
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-26 23:23:48 +0100

    Python Annotation - Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Concatenation with Annotation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates strings as one
+
+    Arguments:
+      str1: type string
+      str2: type string
+
+    Returns:
+      concat_string: type string
+    """
+    concat_string: str = str1 + str2
+    return concat_string
