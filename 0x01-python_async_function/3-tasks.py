80fd2c94f83088594cd4eb15132cde8d00f04eee
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-28 00:10:10 +0100

    Python Async - Task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Return an Async function from non-async function"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create and return an Async function from non-async function
+
+    Arguments:
+        max_delay: Delay in secs (type int)
+
+    Returns:
+        Async object: (type _asyncio.Task)
+    
+    """
+    x = asyncio.create_task(wait_random(max_delay))
+    return x
