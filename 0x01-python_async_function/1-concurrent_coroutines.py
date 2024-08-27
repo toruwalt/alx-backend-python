2c5f520166581164b8c6e5f28bf42716c86dd9b8
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-27 22:25:26 +0100

    Python Async - Task 1:Attempt 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Runs 
+
+    Arguments:
+        n: Number of times to run loop (type int)
+        max_delay: Delay in secs (type int)
+
+    Returns:
+        list_delay: A list of delays (type float)
+    
+    """
     list_delay = [await wait_random(max_delay) for x in range(0, n)]
     list_delay.sort()
     return list_delay
