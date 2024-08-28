f04d874950cbbdef5480e8400b443dcbe3b67cbf
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-28 16:51:29 +0100

    Python Async Comprehension - Task 0:Attempt 2

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,6 +5,13 @@ import random
 
 
 async def async_generator():
+    """
+    Runs an Asynchronous Generator
+    
+    Yields:
+        A value of type float
+    """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        x: float = random.uniform(0, 10)
+        yield x
