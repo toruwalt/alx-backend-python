353d0f6655462a9db8e41d36dd6411b7855c0226
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-28 17:00:45 +0100

    Python Async Comprehension - Task 0:Attempt 4

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float]:
     """
     Runs an Asynchronous Generator
 
