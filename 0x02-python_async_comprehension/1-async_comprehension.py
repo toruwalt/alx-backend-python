2e68887556f85f5bd3bb236f20835fd8b76fa914
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-28 23:39:03 +0100

    Python Async Comprehension - Task 1:Attempt 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Async Comprehension"""
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """
     Runs an Asynchronous Generator
 
