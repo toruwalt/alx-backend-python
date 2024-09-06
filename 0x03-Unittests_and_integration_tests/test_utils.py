41b1f6e6b443133a5f71e2d211677ee754929941
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-06 10:18:31 +0100

    UT/IT - Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test method that check assertEqual"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test method that check assertRaises"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
