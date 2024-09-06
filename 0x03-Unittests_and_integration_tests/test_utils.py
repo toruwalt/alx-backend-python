da093d089900b3584917a93e2a526e6aef5a1bc3
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-06 15:03:11 +0100

    UT/IT - Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Parameterize a unit test"""
+import requests
 import unittest
 from parameterized import parameterized
+from unittest.mock  import patch
 
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,3 +29,31 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test method that check assertRaises"""
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Mocks HTTP Calls"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    
+    # @patch('utils.get_json')
+    # def test_get_json(self, url, mock_get, mock_expected):
+        # """Test method for getting JSON"""
+        
+        # mock_get.return_value = mock_expected
+
+        # test_payload = get_json(url)
+        # self.assertEqual(test_payload, mock_expected)
+    
+
+    @patch('utils.get_json')
+    def test_get_json(self, url, mock_expected, mock_get):
+        """Test method for getting JSON"""
+        mock_get.return_value = mock_expected
+
+        test_payload = get_json(url)
+
+        self.assertEqual(test_payload, mock_expected)
+
