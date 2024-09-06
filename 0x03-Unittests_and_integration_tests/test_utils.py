dd3dcf7e5bc8a01f135daf544a941c8ab8c0b6ca
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-06 03:14:25 +0100

    UT/IT - Task 0:Attempt 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 """Parameterize a unit test"""
-"""Create a TestAccessNestedMap class that inherits from unittest.TestCase."""
-import unittest 
+import unittest
 from parameterized import parameterized
 
 access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """UnitTest Class"""
+
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
-        ({"a": {"b": 2}}, ("a",)),
-        ({"a": {"b": 2}}, ("a", "b"))
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    
-    def test_access_nested_map(self, nested_map, expected):
-        self.assertEqual(access_nested_map(nested_map), expected)
-    
-    
-        
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test method that check assertEqual"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
